55bf49905ff293aaec210a95cd801122e2818280
operationalbi-pixel <operational.bi@gmail.com>
2026-07-29 11:37:12 +0700

    Add files via upload

---
+++ Doc/config.js
@@ -0,0 +1,7 @@
+window.BAKERZIN_CONFIG = Object.freeze({
+  // Tempel URL deployment Google Apps Script Web App yang berakhiran /exec.
+  API_URL: 'https://script.google.com/macros/s/AKfycbwkNjpp4vLmmJ1IFEqNIXH5W8IKDV1hXs6sI7DEH5FJnPZvVxDgF1XyLCk0dXQEm9N7/exec',
+
+  // Kosongkan agar alamat dashboard mengikuti lokasi GitHub Pages saat ini.
+  SITE_BASE_URL: ''
+});
