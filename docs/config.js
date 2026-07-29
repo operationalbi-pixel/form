0d918df53bd350d230cc60a470a6a2e4c700b3cf
operationalbi-pixel <operational.bi@gmail.com>
2026-07-30 00:28:39 +0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,6 @@
 window.BAKERZIN_CONFIG = Object.freeze({
   // Tempel URL deployment Google Apps Script Web App yang berakhiran /exec.
-  API_URL: 'https://script.google.com/macros/s/AKfycbz5AsRU_Hd83MQhDBnECzGKCzMB20PFJQmDjb3VcduJP3iInvjr2rmD2qKkRmTZiCEX/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw3_sWh8ycamWxktXwYiO6v-KeOcN98oJFNqK0QCtD9FnLAhCQJCZKqvOfwKKGdNvqQ/exec',
 
   // Kosongkan agar alamat dashboard mengikuti lokasi GitHub Pages saat ini.
   SITE_BASE_URL: ''
