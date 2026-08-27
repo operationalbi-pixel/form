988ea17359228d2570d961dc2ad9ad0ebc6b5dc9
operationalbi-pixel <operational.bi@gmail.com>
2026-08-27 18:02:37 +0700

    Hide completed task actions and refresh task cache optimistically

--- docs/api-client.js
+++ docs/api-client.js
@@ -1,7 +1,7 @@
 (function (global) {
   'use strict';
 
-  var CHAT_ASSET_VERSION = '20260827-target4-mobile7';
+  var CHAT_ASSET_VERSION = '20260827-target4-mobile8';
 
   function apiUrl() {
     var value = global.BAKERZIN_CONFIG && global.BAKERZIN_CONFIG.API_URL || '';
